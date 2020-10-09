8ed0a33bd56de5f79ed66a5b25eea6fb5eda8869
Pramod <pramod.bhakta@leadschool.in>
2020-10-09 18:50:05 +0530

    created Router js

--- index.js
+++ index.js
@@ -3,41 +3,27 @@
 
 const express = require('express');
 const app = express();
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser');
+const mainRouter = require('./routers');
 
-app.set('PORT', 27777);
-app.get('/', function (req, res, next) {
-    console.log(req);
-    res.send("HELLO WORLD!");
-});
+app.set('PORT', 3000);
 
-app.use(bodyparser());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 
-app.get('/add', addNumbers)
+app.use(bodyParser.urlencoded({
+    limit: '5mb',
+    extended: false
+}));
 
-function addNumbers(req, res, next) {
-    let num1 = Number(req.query.number1);
-    let num2 = Number(req.query.number2);
-    res.send(`Sum of ${num1} and ${num2} = ${num1 + num2}`);
-}
+app.use('/api', mainRouter);
 
-app.post('/addAttendance', insertAttendance)
 
-function insertAttendance(req, res, next) {
-    try {
-        if (req.body.status) {
-            console.log("It is true");
-        }
-
-        //Do some insert operations in db, maybe analysis.
-        res.send({ success: true, msg: "Attendance added successfully" });
-    } catch (err) {
-        console.log(err);
-        res.send({ success: false, msg: "Errored" });
-    }
-}
 
 
 app.listen(app.get('PORT'), function () {
     console.log(`App started on server ${app.get('PORT')}`);
 });
+
+module.exports = app;
